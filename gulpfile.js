2b9784aad21d68fae78507d3385800538fc6c0cd
Stefan Talen <s.r.talen@gmail.com>
2014-11-19 23:40:57 +0100

    Adding source maps and watch command

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp        = require('gulp');
     concat      = require('gulp-concat');
-    sass        = require('gulp-ruby-sass');
+    sass        = require('gulp-sass');
     prefix      = require('gulp-autoprefixer');
     minifyCSS   = require('gulp-minify-css');
     plumber     = require('gulp-plumber');
+    sourcemaps  = require('gulp-sourcemaps');
 
 var destinationFolder = './compiled',
     bowerFolder = './bower_components',
@@ -13,11 +14,20 @@ var destinationFolder = './compiled',
 gulp.task('sass', function() {
     return gulp.src(sassFolder + '/style.scss')
         .pipe(plumber())
-        .pipe(sass({sourcemap:false}))
+        .pipe(sourcemaps.init())
+            .pipe(sass())
         //.pipe(minifyCSS())
-        .pipe(concat('style.min.css'))
-        .pipe(prefix(["last 2 version"]))
+            .pipe(concat('style.min.css'))
+            .pipe(prefix(["last 2 version"]))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest(destinationFolder));
 });
 
+//Watch
+gulp.task('watch', function () {
+  //gulp.watch(files.scripts, ['scripts']);
+  gulp.watch(sassFolder + '/**/!(ie)*.scss', ['sass']);
+});
+
 gulp.task('default', ['sass']);
+gulp.task('dev', ['sass', 'watch']);
