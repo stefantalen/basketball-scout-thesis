ecc1a831ff107cf095ea6dbfd1600f95ac566178
Stefan Talen <s.r.talen@gmail.com>
2015-01-19 00:15:51 +0100

    Generating marker element on press, optimising selection

--- js/test1.js
+++ js/test1.js
@@ -1,3 +1,8 @@
+// Disable page moving
+$(document).ready(function () {
+    $(document).bind('touchmove', false);
+});
+
 var mc = new Hammer.Manager($('.court-background').get(0));
 var pan = new Hammer.Pan();
 var press = new Hammer.Press({time: 10});
@@ -7,25 +12,44 @@ pan.recognizeWith(swipe);
 mc.add([pan, swipe, press]);
 
 mc.on("press", function(Event) {
-    var $marker = $(".marker");
+    // If there is an position marker get it, otherwise create a new one
+    var $marker =
+        $(".court-background .marker.position").length == 1 ?
+        $(".court-background .marker.position") : 
+        $('<div class="marker position"><div class="action-rebound"></div><div class="action-hit"></div><div class="position"></div><div class="action-miss"></div></div>');
     
     $marker.show().css({
         top: Event.center.y - 32,
         left: Event.center.x - 32
     });
+    if ($(".court-background .marker.position").length == 0) {
+        $(".court-background").append($marker);
+    }
     addFeedback("Positie ingevoerd");
 });
-console.log($("body").hasClass( 'interaction' ));
 
-mc.on("swipeleft", function(Event) {
-    console.log('miss');
-});
-mc.on("swiperight", function(Event) {
-    console.log('rebound');
-});
-mc.on("swipedown", function(Event) {
-    console.log('score');
-});
+if (!$("body").hasClass( 'test-1-position' )) {
+    
+    setInterval(function() {
+        $(".marker.position").toggleClass( 'animated' );
+    }, 250);
+    
+    mc.on("swipeleft", function(Event) {
+        addFeedback("Schot mis geregistreerd");
+        var $marker = $(".marker.position").removeClass( 'position' ).addClass( 'missed' );
+    });
+    
+    /*
+    mc.on("swiperight", function(Event) {
+        console.log('rebound');
+    });
+    */
+    
+    mc.on("swipedown", function(Event) {
+        addFeedback('Schot raak geregistreerd');
+        var $marker = $(".marker.position").removeClass( 'position' ).addClass( 'hit' );
+    });
+}
 
 function addFeedback(string) {
     $(".feedback").append("<li>"+ string +"</li>");
